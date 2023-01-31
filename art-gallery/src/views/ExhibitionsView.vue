e16e89bb08e8bd7323164ad6f4886228f1aa8c77
Sarah Gosling <sarahannegosling@gmail.com>
2023-01-31 13:25:28 +1100

    added methods to change formatting of dates in exhibition tables

--- art-gallery/src/views/ExhibitionsView.vue
+++ art-gallery/src/views/ExhibitionsView.vue
@@ -2,6 +2,8 @@
   <div class="exhibitions">
     <HeadingComponent title="EXHIBITIONS"/>
     <TableAntDesign :exhibitions="this.exhibitions" v-on:updateData="updateData()"/>
+    <br>
+    <br>
     <TableExhibition :exhibitions="this.exhibitions" v-on:updateData="updateData()"/>
   </div>
 </template>
@@ -33,16 +35,41 @@ export default {
             .then(data => {
                 this.exhibitions = data;
                 this.fields = Object.keys(this.exhibitions[0]);
+                this.parseDate();
                 this.dataLoaded = true;
             });
         },
-
         // Update data on table item added.
         updateData() {
             this.fetchExhibitions();
+        },
+        // Changes the date strings to Date object.
+        parseDate() {
+          this.exhibitions.forEach(element => {
+            var startDate = new Date(Date.parse(element.startDate));
+            element.startDate = this.formatDate(startDate);
+          });
+          this.exhibitions.forEach(element => {
+            var endDate = new Date(Date.parse(element.endDate));
+            element.endDate = this.formatDate(endDate);
+          });
+        },
+        // Formats Date object.
+        formatDate(dateObject) {
+          var date = dateObject.getDate();
+          var month = dateObject.getMonth();
+          var year = dateObject.getFullYear();
+
+          if (date < 10) {
+              date = '0' + date;
+          }
+
+          if (month < 10) {
+              month = '0' + month;
+          }
+          return dateObject = date + "-" + month + "-" + year;
         }
     },
-
     mounted() {
         this.fetchExhibitions();
     },
