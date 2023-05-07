e347a990d60adf05b48fa9447b530566e9f156ce
agelb <agelbhauer@gmail.com>
2023-05-07 16:03:50 +1000

    generate sitemap.xml

--- art-gallery/vue.config.js
+++ art-gallery/vue.config.js
@@ -1,4 +1,42 @@
 const { defineConfig } = require('@vue/cli-service')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+
+// base url for deployment
+const baseurl = 'https://localhost:8080'
+
+// array containing path objects for sitemap plugin, generates sitemap.xml
+const paths = [
+  {
+    path: '/',
+    changefreq: 'always'
+  },
+  {
+    path: '/artworks',
+    changefreq: 'always'
+  },
+  {
+    path: '/artistofday',
+    changefreq: 'daily'
+  },
+  {
+    path: '/artworkofday',
+    changefreq: 'daily'
+  },
+  {
+    path: '/culture',
+    changefreq: 'daily'
+  },
+  {
+    path: '/exhibition'
+  },
+  {
+    path: '/about'
+  },
+  {
+    path: '/contact'
+  }
+]
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
@@ -10,5 +48,11 @@ module.exports = defineConfig({
         pathRewrite: {'^/api': '/api'}
       },
     }
+  },
+  configureWebpack: {
+    plugins: [
+      // to direct search engine to baseurl/sitemap.xml endpoint add line containing: `Sitemap: <baseurl>/sitemap.xml` to robots.txt in deployment  
+      new SitemapPlugin({ base: baseurl, paths })
+    ]
   }
 })
